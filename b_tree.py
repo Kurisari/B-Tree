42fd17ec9ffbc33c7d82e148c3abfe073a9c58ed
Cristian Aragón <crisarag8.sal@gmail.com>
2024-05-07 10:00:34 -0600

    Método insertar completo con sus métodos necesarios

--- b_tree.py
+++ b_tree.py
@@ -17,11 +17,35 @@ class BTree:
             self.split_child(new_root, 0)
         self.insert_non_full(self.root, k)
     
-    def split_child(self, x, i):
-        pass
-    
     def insert_non_full(self, x, k):
-        pass
+        i = len(x.keys) - 1
+        if x.leaf:
+            x.keys.append(None)
+            while i >= 0 and k < x.keys[i]:
+                x.keys[i + 1] = x.keys[i]
+                i -= 1
+            x.keys[i + 1] = k
+        else:
+            while i >= 0 and k < x.keys[i]:
+                i -= 1
+            i += 1
+            if len(x.child[i].keys) == (2 * self.t) - 1:
+                self.split_child(x, i)
+                if k > x.keys[i]:
+                    i += 1
+            self.insert_non_full(x.child[i], k)
+
+    def split_child(self, x, i):
+        t = self.t
+        y = x.child[i]
+        z = BTreeNode(leaf=y.leaf)
+        x.child.insert(i + 1, z)
+        x.keys.insert(i, y.keys[t - 1])
+        z.keys = y.keys[t: (2 * t) - 1]
+        y.keys = y.keys[0: t - 1]
+        if not y.leaf:
+            z.child = y.child[t: 2 * t]
+            y.child = y.child[0: t - 1]
 
     def delete(self, k):
         # Implementar la eliminación de un valor en el B-Tree
